36ce6e6eddafece15773043268e7c69aede7c26a
manishabhujbal <119038857+splitsvilla@users.noreply.github.com>
2022-11-26 15:05:06 +0530

    committed to feature branch
    hi TL,
    pls review
    
    Co-Authored-By: manishabhujbal <118868386+manishabhujbal@users.noreply.github.com>

--- AllStringPrograms/src/test/java/stringPrograms/Start.java
+++ AllStringPrograms/src/test/java/stringPrograms/Start.java
@@ -1,5 +1,17 @@
 package stringPrograms;
 
 public class Start {
+	
+	public static void main(String[] args) {
+		
+	
+	String str="Manisha";
+	for(int i=str.length()-1;i>=0;i--)
+	{
+		char c=str.charAt(i);
+		System.out.print(c);
+	}
+		
+	}
 
 }
